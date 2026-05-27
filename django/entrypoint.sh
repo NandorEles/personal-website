331bd36c42d28c6a35fd0f2d97e395ed7d72cadd
NandorEles <Nandoreles06@Gmail.com>
2026-05-27 15:35:30 +0000

    Setting up Linux, Nginx, Django, MySQL framework.

---
+++ django/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+echo "==> Running migrations"
+python manage.py migrate --noinput
+
+echo "==> Collecting static files"
+python manage.py collectstatic --noinput
+
+echo "==> Starting Gunicorn"
+exec gunicorn personalwebsite.wsgi:application \ --bind 0.0.0.0:8000 \ --workers 3 \ --timeout 120
